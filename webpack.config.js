93f3cd8f83c6f3dfbe4f343b82924d33951f171c
Marcin Mazur <marcinmazur@MacBook-Air-Olga.local>
2016-10-18 10:00:52 +0200

    Creates boilerplate react app

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+  entry: './main.js',
+  output: {
+    filename: 'index.js',
+    path: './'
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
